2ea58e95a83a98df0c134ff6e5b1411daf22b8fb
rzadp <roopert7@gmail.com>
2023-08-10 09:02:15 +0200

    Add utils

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import {blake2AsHex} from "@polkadot/util-crypto";
+
 /**
  * Matrix handles of the team supporting this project.
  * Currently - Engineering Automation / Opstooling.
@@ -5,3 +7,13 @@
  */
 export const teamMatrixHandles =
   process.env.NODE_ENV === "development" ? [] : ["@przemek", "@mak", "@yuri", "@bullrich"]; // Don't interrupt other people when testing.
+
+/**
+ * blake2-256 hash of the raw proposal text, as described in the [RFC process](https://github.com/polkadot-fellows/RFCs#process).
+ */
+export const hashProposal = (proposal: string) => {
+  return blake2AsHex(proposal, 256)
+}
+
+// https://stackoverflow.com/a/52254083
+export const byteSize = (str: string): number => new Blob([str]).size;
